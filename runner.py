7f7e52e7f43ddb82734804ebbc40008530891690
Fangyu Wu <fywu85@gmail.com>
2019-07-02 20:49:04 -0700

    Add beta simulation code.

--- runner.py
+++ runner.py
@@ -1,5 +1,7 @@
 import subprocess
 import numpy as np
+import os
+import sys
 np.random.seed(42)  # make tests reproducible (TODO: check if this is indeed effective)
 
 if 'SUMO_HOME' in os.environ:
@@ -12,7 +14,7 @@ from sumolib import checkBinary
 import traci
 import sumolib
 
-route_dict = {}
+route_list = []
 
 def init():
     netconvert_cmd = [
@@ -32,19 +34,36 @@ def init():
 
 
 def gen_route(trans_matrix):
-    route = None #TODO generate a route from transition matrix
-    if route is not in route_dict:
-        route_id = route.join('>')
-        route_dict[route] = route_id
+    route = ['in_to1']
+    node_index = 0
+    edge_list = [
+        'in_to1',
+        '1to2',
+        '1to3',
+        '2to3',
+        '2to4',
+        '3to5',
+        '5to4',
+        '4to_out'
+    ]
+    while route[-1] != '4to_out':
+        probs = trans_matrix[
+            node_index*len(edge_list):(node_index+1)*len(edge_list)]
+        next_edge = np.random.choice(edges, 1, p=probs)[0]
+        route.append(next_edge)
+        node_index = edges.index(next_edge)
+    route_id = route.join('>')
+    if route_id not in route_list:
+        route_list.append(route_id)
         traci.route.add(route_id, route)
-    return route_dict[route]
+    return route_id
 
 
 def add_vehicle(veh_index, route_id):
     traci.vehicle.addFull(vehID='veh{:06}'.format(veh_index), typeID='vehicle', route=route_id)
 
 
-def step(veh_index, trans_matrix)
+def step(veh_index, trans_matrix):
     route_id = gen_route(trans_matrix)
     add_vehicle(veh_indx, route_idx)
     traci.simulationStep()
@@ -57,13 +76,21 @@ def close():
 
 if __name__ == '__main__':
     init()
+    p_12 = 0.5
+    p_13 = 0.5
+    p_23 = 0.3
+    p_24 = 0.7
+    p_35 = 1
+    p_54 = 1
     default_trans_matrix = np.array([
-        0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0;
-        0.0, 0.0, 0.3, 0.7, 0.0, 0.0, 0.0;
-        0.0, 0.0, 0.0, 0.0, 0.6, 0.4, 0.0;
-        0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0;
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0;
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        [0.0, p_12, p_13, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, p_23, p_24, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, p_35, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, p_35, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, p_54, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     ]).flatten()
 
     duration = 3600  # number of steps
